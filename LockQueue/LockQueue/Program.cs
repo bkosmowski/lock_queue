aabd21e6e7820b24612e8a36526c70c5d174928d
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-09-16 22:14:41 +0200

    Added synchronized queue with using lock keyword.

---
+++ LockQueue/LockQueue/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace LockQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var synchronizedQueue = new SynchronizedQueue();
+
+            var firstThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    synchronizedQueue.Add(new QueueItem("First thread item"));
+                    Thread.Sleep(1000);
+                }
+            });
+
+            var secondThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    synchronizedQueue.Add(new QueueItem("Second thread item"));
+                    Thread.Sleep(1000);
+                }
+            });
+
+            var thirdThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    synchronizedQueue.ShowAll();
+                    Thread.Sleep(1000);
+                }
+            });
+
+            var fourthThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    synchronizedQueue.Take().Match(
+                        some: item => Console.WriteLine($"Item from queue {item.ItemName}"),
+                        none: () => Console.WriteLine("There is no item on queue"));
+
+                    Thread.Sleep(400);
+                }
+            });
+
+
+            firstThread.Start();
+            secondThread.Start();
+            thirdThread.Start();
+            fourthThread.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LockQueue/LockQueue/SynchronizedQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Optional;
+
+namespace LockQueue
+{
+    public class QueueItem
+    {
+        public QueueItem(string itemName)
+        {
+            ItemName = itemName;
+        }
+
+        public string ItemName { get; }
+    }
+
+    public class SynchronizedQueue
+    {
+        private readonly Queue<QueueItem> _queueItems;
+
+        private static readonly object Lock = new object();
+
+        public SynchronizedQueue()
+        {
+            _queueItems = new Queue<QueueItem>();
+        }
+        public void Add(QueueItem item)
+        {
+            lock (Lock)
+            {
+                _queueItems.Enqueue(item);
+            }
+        }
+
+        public Option<QueueItem> Take()
+        {
+            lock (Lock)
+            {
+                return _queueItems.TryDequeue(out var item) ? item.Some() : Option.None<QueueItem>();
+            }
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine("On queue:");
+            lock (Lock)
+            {
+                foreach (var item in _queueItems)
+                {
+                    Console.WriteLine($"{item.ItemName}");
+                }
+            }
+        }
+    }
+}
